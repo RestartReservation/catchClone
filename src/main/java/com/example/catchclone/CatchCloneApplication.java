3d0a2afb16c25a587acca3488fd18d4c7fb21af5
0empty <117061586+zeroempty2@users.noreply.github.com>
2024-02-18 17:37:14 +0900

    first commit

---
+++ src/main/java/com/example/catchclone/CatchCloneApplication.java
@@ -0,0 +1,13 @@
+package com.example.catchclone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatchCloneApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(CatchCloneApplication.class, args);
+  }
+
+}

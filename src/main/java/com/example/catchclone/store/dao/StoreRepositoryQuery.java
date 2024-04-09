b12516bf157ef66c70f7716d016b9aa4309c4671
0empty <117061586+zeroempty2@users.noreply.github.com>
2024-04-09 20:23:56 +0900

    #10 <feat> 페이징 쿼리 작성

---
+++ src/main/java/com/example/catchclone/store/dao/StoreRepositoryQuery.java
@@ -0,0 +1,9 @@
+package com.example.catchclone.store.dao;
+
+import com.example.catchclone.store.dto.StoreIndexResponseDto;
+import com.example.catchclone.store.dto.StorePageDto;
+import org.springframework.data.domain.Page;
+
+public interface StoreRepositoryQuery {
+  Page<StoreIndexResponseDto> getStores(StorePageDto storePageDto);
+}

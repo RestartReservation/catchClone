becc7acc8a891687a1bbf6f29306a3ab487b2fa7
PriceHoon <saog3151@gmail.com>
2024-04-10 18:04:18 +0900

    [fix]#10_reservation URL fix

--- src/main/java/com/example/catchclone/reservation/controller/ReservationController.java
+++ src/main/java/com/example/catchclone/reservation/controller/ReservationController.java
@@ -21,7 +21,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("ct/reservation")
+@RequestMapping("/ct/reservation")
 public class ReservationController {
 
   private final ReservationService reservationService;

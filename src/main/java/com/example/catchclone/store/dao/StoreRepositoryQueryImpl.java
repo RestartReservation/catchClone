9c8a1bea97a8600106c314b7f44e8d178796186d
0empty <117061586+zeroempty2@users.noreply.github.com>
2024-04-11 21:08:47 +0900

    <fix> 쿼리에서 sortBy동적으로 처리할 수 있게 수정

--- src/main/java/com/example/catchclone/store/dao/StoreRepositoryQueryImpl.java
+++ src/main/java/com/example/catchclone/store/dao/StoreRepositoryQueryImpl.java
@@ -4,7 +4,10 @@ import static com.example.catchclone.store.entity.QStore.store;
 
 import com.example.catchclone.store.dto.StoreIndexResponseDto;
 import com.example.catchclone.store.dto.StorePageDto;
+import com.example.catchclone.store.entity.Store;
+import com.querydsl.core.types.OrderSpecifier;
 import com.querydsl.core.types.Projections;
+import com.querydsl.core.types.dsl.PathBuilder;
 import com.querydsl.core.types.dsl.Wildcard;
 import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
@@ -17,8 +20,10 @@ import org.springframework.data.support.PageableExecutionUtils;
 import org.springframework.transaction.annotation.Transactional;
 
 @RequiredArgsConstructor
-public class StoreRepositoryQueryImpl implements StoreRepositoryQuery{
+public class StoreRepositoryQueryImpl implements StoreRepositoryQuery {
+
   private final JPAQueryFactory jpaQueryFactory;
+
   @Override
   @Transactional(readOnly = true)
   public Page<StoreIndexResponseDto> getStores(StorePageDto storePageDto) {
@@ -26,71 +31,64 @@ public class StoreRepositoryQueryImpl implements StoreRepositoryQuery{
     int starCut = storePageDto.getStarCut();
     List<StoreIndexResponseDto> dtoList;
 
-    if(!Objects.isNull(storePageDto.getSortBy())) dtoList = getStoresAndSortByKeyword(pageable);
-    else if(storePageDto.isCutByStarRate()) dtoList =  getStoresAndCutByStarRate(pageable,starCut);
-    else dtoList = getStoresSortByDesc(pageable);
-
+    if(Objects.nonNull(storePageDto.getSortBy())) dtoList = getStoresAndSortByKeyword(pageable,storePageDto);
+    else if (storePageDto.isCutByStarRate()) dtoList = getStoresAndCutByStarRate(pageable, starCut);
+    else dtoList = getStoresSortByCreatedAtDesc(pageable);
 
     long totalSize = storeCountQuery().fetch().get(0);
 
     return PageableExecutionUtils.getPage(dtoList, pageable, () -> totalSize);
   }
-  private List<StoreIndexResponseDto> getStoresAndSortByKeyword(Pageable pageable){
-    return jpaQueryFactory
-        .select(
-            Projections.bean(
-                StoreIndexResponseDto.class
-                ,store.id
-                ,store.storeName
-                ,store.storeLocation
-                ,store.starRate
-            )
-        )
-        .from(store)
-        .setHint("org.hibernate.readOnly", true)
+
+  private List<StoreIndexResponseDto> getStoresAndSortByKeyword(Pageable pageable,StorePageDto storePageDto) {
+    OrderSpecifier<?> orderSpecifier = getOrderSpecifier(storePageDto.getSortBy(),
+        storePageDto.isAsc());
+
+    return query()
+        .orderBy(orderSpecifier)
         .limit(pageable.getPageSize())
         .offset(pageable.getOffset())
         .fetch();
   }
-  private List<StoreIndexResponseDto> getStoresAndCutByStarRate(Pageable pageable, int starCut){
-    return jpaQueryFactory
-        .select(
-            Projections.bean(
-                StoreIndexResponseDto.class
-                ,store.id
-                ,store.storeName
-                ,store.storeLocation
-                ,store.starRate
-            )
-        )
-        .from(store)
+
+  private List<StoreIndexResponseDto> getStoresAndCutByStarRate(Pageable pageable, int starCut) {
+    return query()
         .where(store.starRate.gt(starCut))
-        .setHint("org.hibernate.readOnly", true)
         .limit(pageable.getPageSize())
         .offset(pageable.getOffset())
         .fetch();
   }
-  private List<StoreIndexResponseDto> getStoresSortByDesc(Pageable pageable){
-    return jpaQueryFactory
-        .select(
-            Projections.bean(
-                StoreIndexResponseDto.class
-                ,store.id
-                ,store.storeName
-                ,store.storeLocation
-                ,store.starRate
-            )
-        )
-        .from(store)
-        .setHint("org.hibernate.readOnly", true)
+
+  private List<StoreIndexResponseDto> getStoresSortByCreatedAtDesc(Pageable pageable) {
+    return query()
         .orderBy(store.createdAt.desc())
         .limit(pageable.getPageSize())
         .offset(pageable.getOffset())
         .fetch();
   }
-  //
+
+  private JPAQuery<StoreIndexResponseDto> query(){
+    return jpaQueryFactory
+        .select(
+            Projections.bean(
+                StoreIndexResponseDto.class
+                , store.id
+                , store.storeName
+                , store.storeLocation
+                , store.starRate
+            )
+        )
+        .from(store)
+        .setHint("org.hibernate.readOnly", true);
+  }
   private JPAQuery<Long> storeCountQuery() {
     return jpaQueryFactory.select(Wildcard.count)
         .from(store);
   }
+
+  private OrderSpecifier<?> getOrderSpecifier(String sortBy, boolean isAsc) {
+    PathBuilder<Object> defaultPath = new PathBuilder<>(Store.class, Store.class.getSimpleName());
+    return isAsc ? defaultPath.getString(sortBy).asc() : defaultPath.getString(sortBy).desc();
+  }
+
 }

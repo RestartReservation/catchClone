7b2326d44f935db03467cb24c6a95428f957f69a
0empty <117061586+zeroempty2@users.noreply.github.com>
2024-04-17 23:40:05 +0900

    #2 <fix> urlFix - user -> users

--- src/main/java/com/example/catchclone/user/controller/UserController.java
+++ src/main/java/com/example/catchclone/user/controller/UserController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/ct/user")
+@RequestMapping("/ct/users")
 public class UserController {
 
   private final UserService userService;

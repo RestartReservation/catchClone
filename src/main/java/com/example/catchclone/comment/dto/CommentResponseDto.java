88ea024f563ed48e4e8e1409a931ee9f4dbe11bd
0empty <117061586+zeroempty2@users.noreply.github.com>
2024-05-05 17:37:33 +0900

    #11 <fix> CommentResponseDto record -> class로 변경(queryDsl 사용)

--- src/main/java/com/example/catchclone/comment/dto/CommentResponseDto.java
+++ src/main/java/com/example/catchclone/comment/dto/CommentResponseDto.java
@@ -1,8 +1,19 @@
 package com.example.catchclone.comment.dto;
 
 import java.time.LocalDateTime;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-public record CommentResponseDto(Long CommentId, Long userId, String commentContents, String nickname, LocalDateTime createdAt, Long likeCount) {
-
+@Getter
+@Setter
+@NoArgsConstructor
+public class CommentResponseDto {
+    Long CommentId;
+    Long userId;
+    String commentContent;
+    String nickName;
+    LocalDateTime createdAt;
+    Long likeCount;
 }
 

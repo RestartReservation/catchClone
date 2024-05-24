bc178c8472464e17b52cb0ad949b502ca8470034
0empty <117061586+zeroempty2@users.noreply.github.com>
2024-05-24 21:06:01 +0900

    #2 <feat> 유저 프로필 조회 API작성

--- src/main/java/com/example/catchclone/user/controller/UserController.java
+++ src/main/java/com/example/catchclone/user/controller/UserController.java
@@ -1,11 +1,19 @@
 package com.example.catchclone.user.controller;
 
 import com.example.catchclone.common.dto.StatusResponseDto;
+import com.example.catchclone.security.UserDetailsImpl;
 import com.example.catchclone.user.dto.UserLoginRequestDto;
+import com.example.catchclone.user.dto.UserProfileResponseDto;
 import com.example.catchclone.user.dto.UserRequestDto;
 import com.example.catchclone.user.service.UserService;
 import jakarta.servlet.http.HttpServletResponse;
+import java.nio.charset.StandardCharsets;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -28,6 +36,14 @@ public class UserController {
   public StatusResponseDto login(HttpServletResponse httpServletResponse,@RequestBody UserLoginRequestDto userLoginRequestDto){
 
    return userService.login(httpServletResponse,userLoginRequestDto);
+  }
+
+  @GetMapping("/profile")
+  public ResponseEntity<UserProfileResponseDto> getUserProfile(@AuthenticationPrincipal UserDetailsImpl userDetails){
+    UserProfileResponseDto userProfileResponseDto = userService.getUserProfile(userDetails.getUserId());
+    HttpHeaders headers = new HttpHeaders();
+    headers.setContentType(new MediaType("application", "json", StandardCharsets.UTF_8));
+    return ResponseEntity.ok().headers(headers).body(userProfileResponseDto);
   }
 
 }

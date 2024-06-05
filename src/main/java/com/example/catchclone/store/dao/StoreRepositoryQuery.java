1d80abef7a052650831ac9ea8628ace56a5c4f85
2zeroempty@gmail.com <2zeroempty@gmail.com>
2024-06-05 21:41:10 +0900

    #10 <feat> store 상세정보 조회 쿼리 추가

--- src/main/java/com/example/catchclone/store/dao/StoreRepositoryQuery.java
+++ src/main/java/com/example/catchclone/store/dao/StoreRepositoryQuery.java
@@ -1,9 +1,11 @@
 package com.example.catchclone.store.dao;
 
+import com.example.catchclone.store.dto.StoreDetailsResponseDto;
 import com.example.catchclone.store.dto.StoreIndexResponseDto;
 import com.example.catchclone.store.dto.StorePageDto;
 import org.springframework.data.domain.Page;
 
 public interface StoreRepositoryQuery {
   Page<StoreIndexResponseDto> getStores(StorePageDto storePageDto);
+  StoreDetailsResponseDto getStoreDetails(Long storeId);
 }

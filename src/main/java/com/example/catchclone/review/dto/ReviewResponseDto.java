a1449c9c5c4d983b207ea691b6882cdf263d6ec0
2zeroempty@gmail.com <2zeroempty@gmail.com>
2024-07-11 19:27:15 +0900

    #3 <fix> ReviewResponseDto 반환시 isLiked 값 null일 경우 false로 반환하도록 초기화

--- src/main/java/com/example/catchclone/review/dto/ReviewResponseDto.java
+++ src/main/java/com/example/catchclone/review/dto/ReviewResponseDto.java
@@ -22,6 +22,6 @@ public class ReviewResponseDto {
   public String userNickName;
   public String userProfileUrl;
   public Long likeCount;
-  public Boolean isLiked;
+  public Boolean isLiked = false;
   public List<ReviewPictureDto> reviewPictures;
 }
